90d7c6f39d89f4282109c0fbee658d3efcdddbb9
msigurd <101131709+msigurd@users.noreply.github.com>
2026-09-05 18:39:57 +0200

    Output elapsed time to console

--- javascript/puzzle-input-processor.js
+++ javascript/puzzle-input-processor.js
@@ -1,6 +1,9 @@
 import SOLUTION_CLASSES_MAP from './SOLUTION_CLASSES_MAP.js';
 
 self.onmessage = ({ data: { selectedDay, selectedPart, puzzleInput } }) => {
+  const startTime = Date.now();
   const result = SOLUTION_CLASSES_MAP[selectedDay][selectedPart].process(puzzleInput);
+  const elapsedTime = Date.now() - startTime;
+  console.log(`Done in ${elapsedTime / 1000} sec`);
   self.postMessage(result);
 };
